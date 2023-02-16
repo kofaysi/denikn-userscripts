9ce368a88896330debf21a893bc3b37b9f6905c0
kofaysi <17022173+kofaysi@users.noreply.github.com>
2023-02-16 09:12:16 +0100

    The initial upload

---
+++ denikn-audio-speed.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name       Set Audio Playback Speed to 1
+// @namespace  http://your.namespace
+// @version    1
+// @description  Sets the playback speed of the audio to 1.5 on page load
+// @match      https://denikn.cz/*
+// @grant      none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var radioButtons = document.querySelectorAll('input[name="mep_0_speed"]');
+    for (var i = 0; i < radioButtons.length; i++) {
+        if (radioButtons[i].value === "1.50") {
+            radioButtons[i].click();
+            break;
+        }
+    }
+})();
