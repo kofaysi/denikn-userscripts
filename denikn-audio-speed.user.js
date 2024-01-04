34da6a52502898bdfb5df7c6119bc9a75ee977a7
kofaysi <17022173+kofaysi@users.noreply.github.com>
2024-01-04 20:07:59 +0100

    Update denikn-audio-speed.user.js
    Corrected header
    Changed the default playback speed

--- denikn-audio-speed.user.js
+++ denikn-audio-speed.user.js
@@ -1,17 +1,18 @@
 // ==UserScript==
 // @name       Change Audio Playback Speed
-// @namespace  https://github.com/kofaysi/
-// @version    1
+// @namespace  https://github.com/kofaysi/denikn-scripts/edit/main/denikn-audio-speed.user.js
+// @version    1.0
 // @description  Sets the playback speed of the audio to 1.5 on page load
 // @match      https://denikn.cz/*
 // @grant      none
+// @author     https://github.com/kofaysi
 // ==/UserScript==
 
 (function() {
     'use strict';
     var radioButtons = document.querySelectorAll('input[name="mep_0_speed"]');
     for (var i = 0; i < radioButtons.length; i++) {
-        if (radioButtons[i].value === "1.50") {
+        if (radioButtons[i].value === "1.75") {
             radioButtons[i].click();
             break;
         }
