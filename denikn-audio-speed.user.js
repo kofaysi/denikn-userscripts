7c263bcc4c18831557241c1de4941e53ed56851f
kofaysi <17022173+kofaysi@users.noreply.github.com>
2024-09-05 09:10:14 +0200

    Update denikn-audio-speed.user.js
    added saving value to cache

--- denikn-audio-speed.user.js
+++ denikn-audio-speed.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name       Change Audio Playback Speed
-// @namespace  https://github.com/kofaysi/denikn-scripts/edit/main/denikn-audio-speed.user.js
-// @version    2024-01-04
-// @description  Sets the playback speed of the audio to 1.5 on page load
+// @namespace  https://github.com/kofaysi/
+// @version    1.2
+// @description  Sets the playback speed of the audio based on the last saved value in localStorage, defaulting to 1.5
 // @match      https://denikn.cz/*
 // @grant      none
 // @author     https://github.com/kofaysi
@@ -10,11 +10,24 @@
 
 (function() {
     'use strict';
-    var radioButtons = document.querySelectorAll('input[name="mep_0_speed"]');
-    for (var i = 0; i < radioButtons.length; i++) {
-        if (radioButtons[i].value === "1.75") {
-            radioButtons[i].click();
-            break;
+
+    // Function to set the playback speed
+    function setPlaybackSpeed(speed) {
+        var radioButtons = document.querySelectorAll('input[name="mep_0_speed"]');
+        for (var i = 0; i < radioButtons.length; i++) {
+            if (radioButtons[i].value === speed) {
+                radioButtons[i].click();
+                localStorage.setItem('audioPlaybackSpeed', speed); // Save the selected speed to localStorage
+                break;
+            }
         }
     }
+
+    // Get the saved speed from localStorage, default to 1.5 if not set
+    var savedSpeed = localStorage.getItem('audioPlaybackSpeed') || "1.5";
+
+    // Apply the saved speed
+    setPlaybackSpeed(savedSpeed);
+
+    // Optionally, you could allow the user to change the speed and save the new setting here
 })();
