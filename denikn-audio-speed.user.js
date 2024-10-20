71e7ea928d4e643d9637af9c32a9d64c140b3b9c
kofaysi <17022173+kofaysi@users.noreply.github.com>
2024-10-20 22:30:53 +0200

    Update denikn-audio-speed.user.js
    update header

--- denikn-audio-speed.user.js
+++ denikn-audio-speed.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name       Change Audio Playback Speed @denikn.cz
-// @namespace  https://github.com/kofaysi/denikn-scripts/edit/main/denikn-audio-speed.user.js
+// @namespace  https://github.com/kofaysi/denikn-userscripts/blob/main/denikn-audio-speed.user.js
 // @version    1.4
 // @description  Monitors and sets the playback speed of the audio based on the last saved value in localStorage, defaulting to 1.5, and saves any changes during the session
 // @match      https://denikn.cz/*
